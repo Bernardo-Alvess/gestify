ddddf4b69c170ab71d061a77e90269ba34ca0050
Bernardo Alves <bernardoalves330@gmail.com>
2024-09-20 15:01:53 -0300

    cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { router } from './routes';
 import { zodErrorHandler } from './middleware/zod-error-handler';
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
 app.use(zodErrorHandler)
 
