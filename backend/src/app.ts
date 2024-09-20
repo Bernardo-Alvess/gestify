ea965c674dcd895458f2b355c00f70709e88ceb9
Bernardo Alves <bernardoalves330@gmail.com>
2024-09-20 14:15:10 -0300

    validação de dados feitas com os schemas e error handler adicionado

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { router } from './routes';
+import { zodErrorHandler } from './middleware/zod-error-handler';
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
+app.use(zodErrorHandler)
 
 export { app }
--- backend/src/controllers/CompanyController.ts
+++ backend/src/controllers/CompanyController.ts
@@ -1,50 +1,58 @@
+import { ZodError } from "zod";
 import { Company } from "../entities/Company/Company";
+import { createCompanySchema } from "../lib/schemas/company/create-company-schema";
 import { CompanyRepository } from "../repositories/implementations/CompanyRepository"
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 
 class CompanyController {
     constructor(
         private repository: CompanyRepository
     ) { }
 
-    async createCompany(req: Request, res: Response) {
+    async createCompany(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { email, password, name, corporateReason, cnpj } = req.body;
+            const company = new Company({ email, password, name, corporateReason, cnpj });
+            await this.repository.createCompany(company)
 
-        const { email, password, name, corporateReason, cnpj } = req.body;
-
-        const company = new Company({ email, password, name, corporateReason, cnpj });
-
-        await this.repository.createCompany(company)
-
-        res.json({
-            company: {
-                email, password, name, corporateReason, cnpj
-            }
-        })
+            res.json({
+                company: {
+                    email, password, name, corporateReason, cnpj
+                }
+            })
+        } catch (e) {
+            next(e)
+        }
     }
 
-    async deleteCompany(req: Request, res: Response) {
-        const companyId = req.params.id
-
-        console.log(req.params.id)
-
-        await this.repository.deleteCompany(companyId);
-
-        res.json({ msg: "deleted" })
-    }
-
-    async getCompanies(req: Request, res: Response) {
-
-        const companyes = await this.repository.getCompanies();
-        res.json({ companyes })
+    async deleteCompany(req: Request, res: Response, next: NextFunction) {
+        try {
+            const companyId = req.params.id
+            await this.repository.deleteCompany(companyId);
+            res.json({ msg: "deleted" })
+        } catch (e) {
+            next(e)
+        }
 
     }
 
-    async updateCompany(req: Request, res: Response) {
-        const { id, email, password, name, corporateReason } = req.body
+    async getCompanies(req: Request, res: Response, next: NextFunction) {
+        try {
+            const companyes = await this.repository.getCompanies();
+            res.json({ companyes })
+        } catch (e) {
+            next(e)
+        }
+    }
 
-        const updatedCompany = await this.repository.updateCompany(id, { email, password, name, corporateReason })
-
-        res.json({ updatedCompany })
+    async updateCompany(req: Request, res: Response, next: NextFunction) {
+        try {
+            const { id, email, password, name, corporateReason } = req.body
+            const updatedCompany = await this.repository.updateCompany(id, { email, password, name, corporateReason })
+            res.json({ updatedCompany })
+        } catch (e) {
+            next(e)
+        }
     }
 }
 
