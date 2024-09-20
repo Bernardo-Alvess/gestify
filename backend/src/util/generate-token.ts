47a2d6f65eed8b6bd88eff411ba77d341c620c2d
Bernardo Alves <bernardoalves330@gmail.com>
2024-09-20 14:58:49 -0300

    adicionado vida util de 3 dias para o token

--- backend/src/util/generate-token.ts
+++ backend/src/util/generate-token.ts
@@ -9,7 +9,7 @@ interface props {
 
 const generateToken = ({ id, ownerId }: props) => {
     const secretKey = process.env.JWT_SECRET ?? 'abc123'
-    const token = jwt.sign({ id, ownerId }, secretKey)
+    const token = jwt.sign({ id, ownerId }, secretKey, { expiresIn: '72h' })
     return token
 }
 
