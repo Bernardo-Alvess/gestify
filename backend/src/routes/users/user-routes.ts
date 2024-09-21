91e2a2d8920268dba741e352bdbeae126a300c82
Bernardo Alves <bernardoalves330@gmail.com>
2024-09-21 19:39:58 -0300

    commit

--- backend/src/routes/users/user-routes.ts
+++ backend/src/routes/users/user-routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { userController } from "../../controllers/UserController";
+import { z } from "zod";
 
 const userRouter = Router()
 
@@ -10,10 +11,15 @@ userRouter.post('/', (req, res, next) => {
 userRouter.get('/', (req, res, next) => {
     userController.getUsers(req, res, next)
 })
+
 userRouter.get('/:id', (req, res, next) => {
+    const idSchema = z.string().uuid()
+    idSchema.parse(req.params.id)
     userController.getUserById(req, res, next)
 })
 userRouter.delete('/:id', (req, res, next) => {
+    const idSchema = z.string().uuid()
+    idSchema.parse(req.params.id)
     userController.deleteUser(req, res, next)
 })
 userRouter.put('/', (req, res, next) => {
