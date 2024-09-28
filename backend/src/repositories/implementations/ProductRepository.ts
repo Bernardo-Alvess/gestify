c0ab42fecc3d4eb1b3c5cc54f90b278e142b7cbd
Vitor Bernardino <vitor.hernandorena@hotmail.com>
2024-09-27 21:15:06 -0300

    crud produtos com bug

---
+++ backend/src/repositories/implementations/ProductRepository.ts
@@ -0,0 +1,47 @@
+
+import { prisma } from "../../lib/prisma";
+import { Product } from '../../entities/Products/Product';
+import { IProductRepository } from '../IProductRepository';
+import { IUpdateProductDto } from '../../entities/Products/dtos/IUpdateProductDto'; 
+
+export class ProductRepository implements IProductRepository {
+
+    async getProduct(id: string): Promise<Product | undefined> {
+        const data = await prisma.product.findUnique({
+          where: { id },
+        });
+    
+        if (data) return new Product({ ...data });
+    
+        return undefined;
+      }
+
+      async updateProduct(id: string, productData: IUpdateProductDto): Promise<Product | undefined> {
+        await prisma.product.update({
+          where: { id },
+          data: {
+            ...productData,
+          },
+        });
+    
+        const product = this.getProduct(id);
+        return product;
+      }
+
+      async getProducts(): Promise<Product[]> {
+        return await prisma.product.findMany();
+      }
+
+      async createProduct(product: Product){
+        await prisma.product.create({
+          data: product ,
+        });
+      }
+
+      async deleteProduct(id: string): Promise<void> {
+        await prisma.product.delete({
+          where: { id },
+        });
+      }
+
+}
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { companyRouter } from "./company/company-routes";
+import { productRouter } from "./products/product-routes";
 
 const router = Router()
 
 router.use('/company', companyRouter)
+router.use('/products', productRouter)
 
 export { router }
