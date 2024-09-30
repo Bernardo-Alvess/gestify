42ff436f0ea55763683ad74df80a32f3dcbd7682
Bernardo Alves <bernardoalves330@gmail.com>
2024-09-30 19:20:33 -0300

    porra da rota de product ta AQUI Ó

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import { userRouter } from "./user/user-routes";
 import { authRouter } from "./auth/auth-routes";
 import { serviceOrderRouter } from "./service-order/servicer-orders-routes";
 import { statusRouter } from "./status/status-routes";
+import { productRouter } from "./products/product-routes";
 
 const router = Router()
 
@@ -12,4 +13,6 @@ router.use('/api/user', userRouter)
 router.use('/api/auth', authRouter)
 router.use('/api/service-order', serviceOrderRouter)
 router.use('/api/status', statusRouter)
+router.use('/api/product', productRouter)
+
 export { router }
