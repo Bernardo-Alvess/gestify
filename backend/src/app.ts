e09444c04dbdbe429a9298052eec3e7cb62b1258
Bernardo Alves <bernardoalves330@gmail.com>
2024-11-18 20:56:12 -0300

    commit

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://gestify-frontend.onrender.com', 'https://gestify-wheat.vercel.app/'],
+    origin: ['http://localhost:5173', 'https://gestify-frontend.onrender.com', 'https://gestify-wheat.vercel.app/', '*'],
     credentials: true
 }))
 app.use(router)
