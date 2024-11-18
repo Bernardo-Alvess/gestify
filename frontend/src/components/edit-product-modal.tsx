a57151879383945a96204113b0c5295f7be9e8cf
Bernardo Alves <bernardoalves330@gmail.com>
2024-11-18 20:42:26 -0300

    import

--- frontend/src/components/edit-product-modal.tsx
+++ frontend/src/components/edit-product-modal.tsx
@@ -207,7 +207,6 @@
 import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { toast } from 'sonner';
-import { addProductToSo } from '../data/products-so';
 import { getProductForSo } from '../http/get-product-for-so';
 import { updateProductServiceOrder } from '../http/update-product-service-order';
 
