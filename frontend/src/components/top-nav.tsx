679a7dd1325bc84d88c3a2002569838d7370c311
Bernardo Alves <bernardoalves330@gmail.com>
2024-12-02 14:31:20 -0300

    feat: remove top nav

--- frontend/src/components/top-nav.tsx
+++ frontend/src/components/top-nav.tsx
@@ -4,26 +4,28 @@ import IconNotification from '../public/assets/home-page/icons/generic/notificat
 
 const TopNav = () => {
     return (
-        <nav>
-            <ul className='flex space-x-5'>
-                <li>
-                    <a href="#settings">
-                        <img className='w-7 bg-white rounded-md p-1' src={IconSettings} alt="icone configuracoes" />
-                    </a>
-                </li>
-                <li>
-                    <a href="#notifications">
-                        <img className='w-7 bg-white rounded-md p-1' src={IconNotification} alt="icone notificacoes" />
-                    </a>
-                </li>
-                <li>
-                    <a href="#profile">
-                        <img className='w-7 rounded-lg' src={CachorroTeste} alt="foto de perfil" />
-                    </a> {/* tirar essa foto de cachorro daqui */}
-                </li>
-            </ul>
-        </nav>
-    )
+		// <nav>
+		//     <ul className='flex space-x-5'>
+		//         <li>
+		//             <a href="#settings">
+		//                 <img className='w-7 bg-white rounded-md p-1' src={IconSettings} alt="icone configuracoes" />
+		//             </a>
+		//         </li>
+		//         <li>
+		//             <a href="#notifications">
+		//                 <img className='w-7 bg-white rounded-md p-1' src={IconNotification} alt="icone notificacoes" />
+		//             </a>
+		//         </li>
+		//         <li>
+		//             <a href="#profile">
+		//                 <img className='w-7 rounded-lg' src={CachorroTeste} alt="foto de perfil" />
+		//             </a> {/* tirar essa foto de cachorro daqui */}
+		//         </li>
+		//     </ul>
+		//     <div></div>
+		// </nav>
+		<div></div>
+	);
 }
 
 export default TopNav
